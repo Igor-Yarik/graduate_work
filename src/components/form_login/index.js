befb0b995bb859bf719aceab28af1c242d65e949
Igor Yaroshovets <igor.yarik@gmail.com>
2023-03-23 20:30:03 +0200

    add div form login

--- src/components/form_login/index.js
+++ src/components/form_login/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import './styles.css'
 import logo from '../../images/logo.svg'
 
-const FormLogin = () => { 
+const FormLogin = () => {
     return (
-        <form className="form_login">
+        <div className="form_login">
             <img className="form_login_img" src={logo} alt="logo_images"></img>
-            <input className="form_login_item" type="text" placeholder="User Name"></input>
-            <input className="form_login_item" type="text" placeholder="Password"></input>
-            <button className="form_login_btn">Login</button>
-        </form>
+            <form className="form_login_items">
+                <input className="form_login_item" type="text" placeholder="User Name"></input>
+                <input className="form_login_item" type="text" placeholder="Password"></input>
+                <button className="form_login_btn">Login</button>
+            </form>
+        </div>
+
     )
 }
 
